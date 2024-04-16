83bf4475842a1c82c4461a7e83ba6ca6c335f66e
Srinath Pal <srinath@sltecherpsolution.com>
2024-04-16 19:52:33 +0530

    leave cron

--- insabhi_sales/models/hr_employee.py
+++ insabhi_sales/models/hr_employee.py
@@ -11,14 +11,32 @@ class HrExtend(models.Model):
     @api.model
     def create(self, vals):
         hr = super(HrExtend, self).create(vals)
+        self.create_leave_allocation(hr)
+        return hr
+
+    @api.model
+    def create_leave_allocation_for_old_employee(self):
+        employees = self.search([])
+        for emp in employees:
+            leave_id = self.env['hr.holidays'].search([('employee_id', '=', emp.id), ('holiday_status_id', '=', self.env.ref('hr_holidays.holiday_status_cl').id),('number_of_days_temp', '=', 18.0)])
+            if not leave_id:
+                self.create_leave_allocation(emp)
+
+    def create_leave_allocation(self, employee, number_of_leaves=18.0):
         leave_id = self.env['hr.holidays'].sudo().create({
             'name': 'Annual Leave ' + str(datetime.datetime.now().year),
             'type': 'add',
             'holiday_type': 'employee',
-            'employee_id': hr.id,
-            'number_of_days_temp': 18,
+            'employee_id': employee.id,
+            'number_of_days_temp': number_of_leaves,
             'holiday_status_id': self.env.ref('hr_holidays.holiday_status_cl').id,
-            'department_id': hr.department_id.id,
+            'department_id': employee.department_id.id,
         })
         leave_id.holidays_confirm()
-        return hr
+
+    @api.model
+    def carry_forward_leaves(self):
+        emp_ids = self.search([])
+        for emp in emp_ids:
+            total_leaves_for_next_year = emp.remaining_leaves + 18.0
+            self.create_leave_allocation(emp, total_leaves_for_next_year)
